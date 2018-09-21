49841cc036a6d9da596f99a035eadb193e782de5
jakejack13 <jckawesome13@gmail.com>
2018-09-20 20:16:50 -0400

    Removed defunct experimental modules

--- Python/JayJay-Bot-Py.py
+++ Python/JayJay-Bot-Py.py
@@ -1,6 +1,4 @@
 import discord
-import asyncio
-import random
 TOKEN = "NDAwMTE5NTUzMTM4NjIyNDg0.DTXBAQ.XZ_UFCOySPa7MwVT37krX3gis-E"
 client = discord.Client()
 
@@ -14,24 +12,6 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!private'):
-        msg = "Hello {0.author.mention}".format(message)
-        await client.send_message(message.author,msg)
-
-async def my_background_task():
-    await client.wait_until_ready()
-    counter = 0
-    channel = discord.Object(id='363801910970548266')
-    while not client.is_closed:
-        counter += 1
-        songs = open("songs.txt", "r")
-        song_num = random.randint(0,20)
-        song = songs.readlines()
-        msg = song[song_num]
-        await client.send_message(channel, msg)
-        await asyncio.sleep(60) # task runs every 60 seconds
 
 @client.event
 async def on_ready():
