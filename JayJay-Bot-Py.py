45811b9330cb0d2b237b691994a2ffd98f9b8d69
Jacob Kerr <jck268@cornell.edu>
2021-01-31 17:31:43 -0500

    Added !name, !help

--- JayJay-Bot-Py.py
+++ JayJay-Bot-Py.py
@@ -25,38 +25,53 @@ async def on_message(message):
         await message.channel.send(msg)
     if message.content.startswith('!game'):
         split = message.content.split(' ')
-        msg = split[1]
+        msg = " ".join(split[1:])
         await client.change_presence(activity=discord.Game(name=msg))
         await message.add_reaction('\U0001F44D')
-    if message.content.startswith('!punish'):
+    if message.content.startswith('!name') :
         split = message.content.split(' ')
-        user = split[1]
-        msg = 'Punishment time'.format(message)
-        if user == "this" :
-            while True :
-                await message.channel.send(msg)
-        else :
-            while True :
-                await message.guild.get_member(user).send(msg)
+        name = " ".join(split[1:])
+        await message.guild.get_member(client.user.id).edit(nick=name)
+        await message.add_reaction('\U0001F44D')
+    #if message.content.startswith('!punish'):
+    #    split = message.content.split(' ')
+    #    user = split[1]
+    #    msg = 'Punishment time'.format(message)
+    #    if user == "this" :
+    #        while True :
+    #            await message.channel.send(msg)
+    #    else :
+    #        while True :
+    #            await message.guild.get_member(user).send(msg)
     if message.content.startswith('!kill') :
         msg = '\U0001F44D'.format(message)
         await message.channel.send(msg)
         await client.logout()
         await client.close()
-    if message.content.startswith('!nerd') :
-        split = message.content.split(' ')
-        other_user = split[1]
-        first_user = message.author.mention
-        rint = random.randint(0,1)
-        if rint == 0 :
-            msg = "" + first_user + " is nerdier than " + other_user
-        else :
-            msg = "" + other_user + " is nerdier than " + first_user
-        await message.channel.send(msg)
+    #if message.content.startswith('!nerd') :
+    #    split = message.content.split(' ')
+    #    other_user = split[1]
+    #    first_user = message.author.mention
+    #    rint = random.randint(0,1)
+    #    if rint == 0 :
+    #        msg = "" + first_user + " is nerdier than " + other_user
+    #    else :
+    #        msg = "" + other_user + " is nerdier than " + first_user
+    #    await message.channel.send(msg)
     if message.content.startswith('!sorry') :
         sorry_num+=1
         msg = ("Sorry, everyone. Sorry counter: " + str(sorry_num)).format(message)
         await message.channel.send(msg)
+    #if message.content.startswith('!help') :
+        #msg = """CURRENT COMMANDS:
+#!hello
+#!random
+#!creator
+#!game
+#!name
+#!sorry
+#Secret command to break the bot""".format(message)
+        #await message.channel.send(msg)
 
 @client.event
 async def on_ready():
