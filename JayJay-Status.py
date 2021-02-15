92e73cb105585ab14db8d9e5a91736bb456e18f8
Jacob Kerr <jck268@cornell.edu>
2021-02-15 00:32:13 -0500

    Change to how status updates

--- JayJay-Status.py
+++ JayJay-Status.py
@@ -9,16 +9,19 @@ client = discord.Client()
 
 
 @client.event
+async def on_message(message) :
+    msg = input('> ')
+    await client.change_presence(activity=discord.Game(name=msg))
+    print(msg)
+
+
+
+@client.event
 async def on_ready():
     # message upon ready
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-    while True :
-        msg = input('> ')
-        #msg = "Time"
-        await client.change_presence(activity=discord.Game(name=msg))
 
 client.run(TOKEN)
