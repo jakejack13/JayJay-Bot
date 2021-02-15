c2b9da773b4eaa30bdbde077c5e539883127a02e
Jacob Kerr <jck268@cornell.edu>
2021-02-15 12:17:50 -0500

    Removed input from Status

--- JayJay-Status.py
+++ JayJay-Status.py
@@ -9,19 +9,13 @@ client = discord.Client()
 
 
 @client.event
-async def on_message(message) :
-    msg = input('> ')
-    await client.change_presence(activity=discord.Game(name=msg))
-    print(msg)
-
-
-
-@client.event
 async def on_ready():
     # message upon ready
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+    msg = "the sunset"
+    await client.change_presence(activity=discord.Activity(name=msg,type=discord.ActivityType.watching))
 
 client.run(TOKEN)
